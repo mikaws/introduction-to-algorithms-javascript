85887d44df7106c965cd7b473b2da2a785ce6ba2
Michael <michaelhenriqued@gmail.com>
2022-07-14 00:35:07 -0300

    move insertion sort logic to a function

--- sorting_algorithms/insertion_sort.js
+++ sorting_algorithms/insertion_sort.js
@@ -1,22 +1,21 @@
 const arr = [9, 123, 23, 912, -21, 42, 0, 1, -3];
-const n = arr.length;
 
-console.info(' --- insertion sort --- \n\nunordered array: \n', arr, '\n\nexecution time:');
-
-console.time();
-
-if(arr.length) {
-   for(let i=1; i<n; i++) {
-       let key = arr[i];
-       let j = i-1;
-       while(j>-1 && arr[j] > key) {
-           arr[j+1] = arr[j];
-           j--;
-       }
-       arr[j+1] = key;
-   }
+function insertionSort() {
+    console.info(' --- insertion sort --- \n\nunordered array: \n', arr, '\n\nexecution time:');
+    console.time();
+    
+    for(let i=1; i<arr.length; i++) {
+        let key = arr[i];
+        let j = i-1;
+        while(j>=0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+     
+    console.timeEnd();
+    console.info('\nordered array: \n', arr, '\n\ncomplexity: O(n^2)');
 }
 
-console.timeEnd();
-
-console.info('\nordered array: \n', arr, '\n\ncomplexity: O(n^2)');
+insertionSort();
